04dcd23dd00178c629d1aeb4da27950c1780caa6
Beatriz Jardim <paulabeatrizlj@hotmail.com>
2022-10-25 16:02:34 -0300

    Update corpus.py

--- corpus.py
+++ corpus.py
@@ -1,21 +1,41 @@
-## Paula Beatriz Louback Jardim
+# Paula Beatriz Louback Jardim
 
+# ------------------------------------------------------
+### Enunciado ###
+
+# Sua tarefa será transformar um conjunto de 5 sites, sobre o tema de processamento de linguagem natural em um conjunto de cinco listas distintas de sentenças.
+# Ou seja, você fará uma função que, usando a biblioteca Beautifull Soap, faça a requisição de uma url, e extrai todas as sentenças desta url.
+# Duas condições são importantes:
+
+# a) A página web (url) deve apontar para uma página web em inglês contendo, não menos que 1000 palavras.  
+# b) O texto desta página deverá ser transformado em um array de senteças.  
+ 
+# Para separar as sentenças você pode usar os sinais de pontuação ou as funções da biblibioteca Spacy.
+
+# ------------------------------------------------------
+# Realizando os imports:
 #!python -m spacy download en_core_web_sm
 
 from bs4 import BeautifulSoup
 from requests import get
-import spacy
-from string import punctuation
+from spacy import load
+import string
 
+# ------------------------------------------------------
+# Guardando as referências para todos os artigos em uma lista:
 artigos = ["https://aliz.ai/en/blog/natural-language-processing-a-short-introduction-to-get-you-started/",
            "https://medium.com/nlplanet/two-minutes-nlp-python-regular-expressions-cheatsheet-d880e95bb468",
            "https://hbr.org/2022/04/the-power-of-natural-language-processing",
            "https://www.activestate.com/blog/how-to-do-text-summarization-with-python/",
            "https://towardsdatascience.com/multilingual-nlp-get-started-with-the-paws-x-dataset-in-5-minutes-or-less-45a70921d709"]
 
-#matriz para guardar o vocabulario de cada documento
-matriz = [[], [], [], [], []]
+# ------------------------------------------------------
+# Esse código percorre todos os documentos, criando uma lista com as sentenças de cada documento
+# e ao final, une todas essas listas em uma matriz.
+# Ao mesmo tempo, o código abaixo realiza a mesma atividade descrita acima para o vocabulário de cada documento,
+# criando uma lista de palavras para cada um e juntando essas listas no final. 
 
+matriz_palavras = [[], [], [], [], []]
 matriz_sentencas = []
 
 i = 0
@@ -28,23 +48,28 @@ for site in artigos:
 
   soup = BeautifulSoup(r, 'html.parser')
   text = soup.find_all('p')
-  nlp = spacy.load("en_core_web_sm")
+  nlp = load("en_core_web_sm")
 
   for paragraph in text:
     content = paragraph.get_text()
     sentences = nlp(content).sents
 
     for sent in sentences:
+      sent = sent.text.strip(string.punctuation)
+      sent = sent.strip(string.digits)
+      sent = sent.strip('\n')
       sents_list.append(sent)
-      words = sent.text.split(" ")
+      words = sent.split(" ")
       
       for word in words:
-        word = word.strip(punctuation)
+        word = word.strip(string.punctuation)
+        word = word.strip(string.digits)
+        word = word.strip('\n')
         document_words.add(word)
 
   matriz_sentencas.append(sents_list)
   for w in document_words:
-    matriz[i].append(w)
+    matriz_palavras[i].append(w)
 
   i += 1
 
@@ -53,4 +78,5 @@ for doc in matriz_sentencas:
   print(f"Site {count}")
   print(f"Quantidade de sentenças: {len(doc)}")
   print(doc)
+  print('\n')
   count += 1
